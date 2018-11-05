8b77759ed3c5226415afe61946563acb151ec707
Felix Sawicki <fmsawicki@gmail.con>
2018-11-05 09:37:39 -0500

    data-types passed, figured out the syntax error preventing my dictionary-literal from working

--- data_types.py
+++ data_types.py
@@ -4,13 +4,25 @@
 #     - "name" -> data_type: string, value: CR3
 #     - "windows_count" -> data_type: int, value: 8
 #     - "glare" -> data_type: boolean, value: True
+class dictionary_classroom:
+    def __init__ (self, chairs, name, windows_count, glare):
+        self.name = name
+        self.chairs = chairs
+        self.windows_count = windows_count
+        self.glare = glare
 
+chairs = 35
+name = 'CR3'
+windows_count = 8
+glare = True
+
+    
 
 # #2: Create a list called "num_list" withs values 1 through 4 in it:
 
-
+num_list = [1,2,3,4]
 # #3: Create a list called "teacher_list" with the strings "zakk", "don",
 # "jimmy", and "hector" in it:
 
-
+teacher_list = ['zakk', 'don', 'jimmy', 'hector']
 # Commit when you finish working on these questions!
